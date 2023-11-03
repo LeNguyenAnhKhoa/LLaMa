752c3230f919d0c68a728b2868ac2827e1b10fa8
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2023-11-03 09:55:28 +0700

    Delete LLaMa directory

--- LLaMa/layers/encoder.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, LayerNormalization, Dropout
-from transformer.layers.multi_head_attention import MultiHeadAttention
-from transformer.layers.position_wise_feed_forward_network import ffn
